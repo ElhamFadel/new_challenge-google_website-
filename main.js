3a4328f8a6bdaed333a750d6de1fe179b4eb1726
ElhamFadel <elham2000Fadel@gmail.com>
2020-09-04 16:14:15 +0300

    final

--- main.js
+++ main.js
@@ -14,27 +14,34 @@ function down(obj){
 
 }
 // code from w3 with simple my change
-var slideIndex = 2;
-showDivs(slideIndex);
+var slideIndex = 1;//هيك انا بقولوا بالوضع الطبيعي 3 سلايدات 
+showDivs(slideIndex);//هيك بعتتها 
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("video");
-  if (n > x.length) {slideIndex = 1 }
+  
+  var x = document.getElementsByClassName("video-container");//هيك حصلت ع الديف نفسه الي فيه الفيديو 
+  if (n > x.length) {slideIndex = 1 }//هيك علشان هو بزيد زيادة فما بدي اياه يقل عن واحد ولا يزيد عن ستة يعني اذا صار اكبر من طوله رجعه واحد 
   if (n < 1) {slideIndex = x.length}
-  for (i = slideIndex; i <= x.length; i++) {
-    x[i+1].style.display = "none";  
+  for(i=0;i<x.length;i++){
+    x[i].style.display = "none"; 
   }
-  for (i = slideIndex; i < 0; i--) {
-    if (i > x.length) {slideIndex = 1 }
-    if (i < 1) {slideIndex = x.length}
+  var n =slideIndex;
+  for (var i = 0; i<3; i++) {
+   
+    x[n-1].style.display = "inline-block"; 
+   n=n+1;
 
-    x[i+1].style.display = "block";  
+    if (n > x.length) {n = 1 }
+//وهان تقريبا خلصت من الجافا سكريبت لل الفيديوهات   وكورونا لسا ما خلصتيش  
+    
   }
+  
+     
+  
   //x[slideIndex-1].style.display = "block";  
 }
 
